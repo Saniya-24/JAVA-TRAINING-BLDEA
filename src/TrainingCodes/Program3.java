6cf92a42839abbb8de4ccaa87a3f629717cf5706
mujaw <mujaw@LAPTOP-AAREAMSJ>
2023-03-20 18:05:41 +0530

    codes

---
+++ src/TrainingCodes/Program3.java
@@ -0,0 +1,18 @@
+package TrainingCodes;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=1;
+        for(int i=1; i<=5; i++) {
+        	for(int j=1; j<=5; j++) {
+        		System.out.print(count+" "); 
+        		count+=1;
+        }
+        	System.out.println();
+	}
+
+	}
+
+}
