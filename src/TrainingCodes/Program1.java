c1e06fd39c292b8beb38ec0b1022fd5f728df5be
mujaw <mujaw@LAPTOP-AAREAMSJ>
2023-03-20 18:01:56 +0530

    commit

---
+++ src/TrainingCodes/Program1.java
@@ -0,0 +1,16 @@
+package TrainingCodes;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=25;
+        for(int i=1; i<=5; i++) {
+        		System.out.print(count+" "); 
+        		count-=5;
+        }
+        	System.out.println();
+
+	}
+
+}
