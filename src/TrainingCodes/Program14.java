dbabc351a2c5e5a6995bda1c1c7eacc6baaf9664
mujaw <mujaw@LAPTOP-AAREAMSJ>
2023-03-22 20:46:12 +0530

    Array code

---
+++ src/TrainingCodes/Program14.java
@@ -0,0 +1,24 @@
+package TrainingCodes;
+
+import java.util.Scanner;
+
+public class Program14 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the count of Students:");
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		int arr[] =new int[n];
+		for(int i=0;i<n;i++) {
+			System.out.println("Enter the marks of student "+(i+1));
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++) {
+			System.out.println("The marks of student "+(i+1)+ " is: "+arr[i]);
+			
+		}
+
+	}
+
+}
