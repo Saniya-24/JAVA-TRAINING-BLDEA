d4d698ca7944ee2e909990bef32a02678c8477c0
mujaw <mujaw@LAPTOP-AAREAMSJ>
2023-03-26 16:52:47 +0530

    Palindrome

---
+++ src/TrainingCodes/Program20.java
@@ -0,0 +1,24 @@
+package TrainingCodes;
+
+import java.util.Scanner;
+
+public class Program20 {
+
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the string:");
+    String str1=sc.next();
+    String str2="";
+    
+    for(int i=str1.length()-1;i>=0;i--) {
+    	str2=str2+str1.charAt(i);
+    }
+    if((str1.equals(str2))==true){
+    	System.out.println("Palindrome");
+    }else {
+    	System.out.println("Not Palindrome");
+    }
+
+	}
+
+}
