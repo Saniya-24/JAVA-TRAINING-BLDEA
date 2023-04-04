48965ad9e4afd00206b8ea5c3c555f74ce9f0929
mujaw <mujaw@LAPTOP-AAREAMSJ>
2023-04-04 09:25:34 +0530

    Mutable Strings

---
+++ src/TrainingCodes/Program25.java
@@ -0,0 +1,12 @@
+package TrainingCodes;
+
+public class Program25 {
+
+	public static void main(String[] args) {
+		StringBuffer sb1=new StringBuffer("rama");
+		StringBuffer sb2=new StringBuffer("sita");
+		
+		sb1.append(sb2);
+		System.out.println(sb1);
+	}
+}
