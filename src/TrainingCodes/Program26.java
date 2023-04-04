e0a01de016a47b4ee21905508c5e9655d82e499f
mujaw <mujaw@LAPTOP-AAREAMSJ>
2023-04-04 09:26:19 +0530

    Shadowing Problem

---
+++ src/TrainingCodes/Program26.java
@@ -0,0 +1,30 @@
+package TrainingCodes;
+
+class Dog{
+	private String name;
+	private String color;
+	private int cost;
+	
+	void setData(String a,String b,int c) {
+		name=a;
+		color=b;
+		cost=c;
+	}
+	
+	void getData() {
+		System.out.println(name);
+		System.out.println(color);
+		System.out.println(cost);
+		
+	}
+}
+
+public class Program26{
+	public static void main(String[] args) {
+		Dog d1=new Dog();
+		d1.setData("Jimmy", "Black", 7000);
+		d1.getData();
+
+	}
+
+}
